abe5bac024da800a63bce2e63fe855d295d6d979
Juan Gorosito <jpablo.gorosito87@gmail.com>
2024-01-16 17:50:53 -0300

    Modificando pairTelefono

--- Java/20240116-GOROSITO-JAVA/src/PairTelefono.java
+++ Java/20240116-GOROSITO-JAVA/src/PairTelefono.java
@@ -6,11 +6,34 @@ public class PairTelefono {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Ingrese un conjunto de números a convertir");
 		String numeros = scanner.nextLine();
-		
-		String [] fragmento = numeros.split(" ");
-		for(int i =0; i<fragmento.length;i++) {
-			System.out.println(fragmento[i]);
+		char[] unaCifra = { 'a', 'd', 'g', 'j', 'm', 'p', 't', 'w' };
+		char[] dosCifras = { 'b', 'e', 'h', 'k', 'n', 'q', 'u', 'x' };
+		char[] tresCifras = { 'c', 'f', 'i', 'l', 'o', 'r', 'v', 'y' };
+		char[] cuatroCifras = { 's', 'z' };
+		String palabra = "";
+		String[] fragmento = numeros.split(" ");
+		for (int i = 0; i < fragmento.length; i++) {
+			int index = Character.getNumericValue(fragmento[i].charAt(0)) - 2;
+
+			if (index == -2) {
+				palabra += " ";
+			} else if (fragmento[i].length() == 1) {
+				palabra += unaCifra[index];
+			} else if (fragmento[i].length() == 2) {
+				palabra += dosCifras[index];
+			} else if (fragmento[i].length() == 3) {
+				palabra += tresCifras[index];
+			} else if (fragmento[i].length() == 4) {
+				if (index == 5) {
+					palabra += cuatroCifras[0];
+				} else {
+					palabra += cuatroCifras[1];
+				}
+			}
+			scanner.close();
+			
 		}
+		System.out.println(palabra);
 	}
 
 }
